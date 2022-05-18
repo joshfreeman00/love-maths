c796309d72cd7a6a2679f03b889e4836495b5d12
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-05-18 14:05:57 +0000

    Add code that creates to random numbers for the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,28 @@
+// wait for the dom to finish loading before playing the game
+// get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+    for (button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submi") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+
+            }
+        })
+    }
+})
+/**
+ * The main game "loop", called whe the script is loaded and afer the users answer has ben processed
+ */
 function runGame() {
 
+    // Generates a random number between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
