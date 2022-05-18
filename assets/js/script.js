ccd73a2a19de2b52a93b83c2aa18f5eae7ffe78c
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-05-18 16:51:18 +0000

    Add code for correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,8 +37,13 @@ function runGame(gameType) {
 function checkAnswer() {
 
 }
-
+/**
+ * gets the numbers (operand) and the operator (plus, minus etc) directly from the DO and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
 
 }
 
