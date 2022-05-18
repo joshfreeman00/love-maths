b77b121c3d0986b3c9dcf48b112af555582ed7da
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-05-18 15:12:31 +0000

    Add more code for addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,29 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
 
             }
         })
     }
+
+    runGame("addition");
 })
 /**
- * The main game "loop", called whe the script is loaded and afer the users answer has ben processed
+ * The main game "loop", called whe the script is loaded and afer the users answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
     // Generates a random number between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -41,7 +50,7 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
 }
 
